41e9d907e389cb366380ace8254f599fd640b893
DevPieter <superpieter88@gmail.com>
2022-10-11 14:33:16 +0200

    Made it easier to create toasts.

--- src/main/java/nl/devpieter/falsereality/Toasts/TestToast.java
+++ src/main/java/nl/devpieter/falsereality/Toasts/TestToast.java
@@ -12,7 +12,6 @@ public class TestToast extends IToast {
     protected Visibility drawToast(MatrixStack matrices, ToastManager manager, long startTime) {
         manager.getGame().textRenderer.draw(matrices, new LiteralText("Hello World!"), 30.0F, 7.0F, new Color(254, 254, 63).getRGB());
         manager.getGame().textRenderer.draw(matrices, new LiteralText("Bottom Text?"), 30.0F, 18.0F, new Color(190, 190, 190).getRGB());
-
         return this.getDefaultShowProgress(startTime);
     }
 }
