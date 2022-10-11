2bedab9e8912d5c457f12f85d0934626fea27c49
DevPieter <superpieter88@gmail.com>
2022-10-11 14:09:48 +0200

    Made it easier to create toasts.

---
+++ src/main/java/nl/devpieter/falsereality/Toasts/IToast.java
@@ -0,0 +1,26 @@
+package nl.devpieter.falsereality.Toasts;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.toast.Toast;
+import net.minecraft.client.toast.ToastManager;
+import net.minecraft.client.util.math.MatrixStack;
+
+public abstract class IToast implements Toast {
+
+    @Override
+    public Visibility draw(MatrixStack matrices, ToastManager manager, long startTime) {
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        manager.drawTexture(matrices, 0, 0, 0, 0, this.getWidth(), this.getHeight());
+
+        return drawToast(matrices, manager, startTime);
+    }
+
+    protected abstract Visibility drawToast(MatrixStack matrices, ToastManager manager, long startTime);
+
+    protected Visibility getDefaultShowProgress(long startTime) {
+        return startTime >= 5000L ? Visibility.HIDE : Visibility.SHOW;
+    }
+}
--- src/main/java/nl/devpieter/falsereality/Toasts/TestToast.java
+++ src/main/java/nl/devpieter/falsereality/Toasts/TestToast.java
@@ -1,26 +1,18 @@
 package nl.devpieter.falsereality.Toasts;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.toast.Toast;
 import net.minecraft.client.toast.ToastManager;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
 
 import java.awt.*;
 
-public class TestToast implements Toast {
+public class TestToast extends IToast {
 
     @Override
-    public Visibility draw(MatrixStack matrices, ToastManager manager, long startTime) {
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderTexture(0, TEXTURE);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        manager.drawTexture(matrices, 0, 0, 0, 0, this.getWidth(), this.getHeight());
-
+    protected Visibility drawToast(MatrixStack matrices, ToastManager manager, long startTime) {
         manager.getGame().textRenderer.draw(matrices, new LiteralText("Hello World!"), 30.0F, 7.0F, new Color(254, 254, 63).getRGB());
-        manager.getGame().textRenderer.draw(matrices, new LiteralText("Bottom Text?"), 30.0F, 18.0F, -1);
+        manager.getGame().textRenderer.draw(matrices, new LiteralText("Bottom Text?"), 30.0F, 18.0F, new Color(190, 190, 190).getRGB());
 
-        return startTime >= 5000L ? Visibility.HIDE : Visibility.SHOW;
+        return this.getDefaultShowProgress(startTime);
     }
 }
