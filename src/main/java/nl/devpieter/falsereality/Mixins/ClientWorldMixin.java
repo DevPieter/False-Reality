adc9ea3773b1e0b9bf2b09a830429c7beec6c3e6
Pieter <51260315+DevPieter@users.noreply.github.com>
2025-07-23 16:41:09 +0200

    ✨ Add time manipulation mixins for ClientWorld and Mouse

---
+++ src/main/java/nl/devpieter/falsereality/Mixins/ClientWorldMixin.java
@@ -0,0 +1,23 @@
+package nl.devpieter.falsereality.mixins;
+
+import net.minecraft.client.world.ClientWorld;
+import nl.devpieter.falsereality.TimeManager;
+import nl.devpieter.falsereality.models.TimeConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ClientWorld.Properties.class)
+public class ClientWorldMixin {
+
+    @Unique
+    private final TimeManager timeManager = TimeManager.getInstance();
+
+    @Inject(at = @At("HEAD"), method = "getTimeOfDay", cancellable = true)
+    public void setTimeOfDay(CallbackInfoReturnable<Long> cir) {
+        TimeConfig timeConfig = this.timeManager.getCurrentTimeConfig();
+        if (timeConfig != null) cir.setReturnValue(timeConfig.getExtraTime());
+    }
+}
